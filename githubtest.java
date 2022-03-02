f362dbd56a8f07ef3a07f45a776698dfdc7037f9
connor2022 <feenconn@gmail.com>
2022-03-02 20:18:30 +1100

    anohter push

---
+++ githubtest.java
@@ -0,0 +1,10 @@
+public class githubtest {
+    
+    public static void main(String[] args) {
+
+        //all the way up to here is setup code no matter what
+        
+                System.out.println("git test");
+            }
+
+}
