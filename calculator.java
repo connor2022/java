8367a106eb6a0240a0b091bce5da24a5aaf0c95a
connor2022 <feenconn@gmail.com>
2022-03-08 03:26:25 +1100

    yessir

--- calculator.java
+++ calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; // this basically puts in code to let there be a input code
 
-public class madbuggycalculator {
+public class calculator {
     public static void main(String[] args) {
 
 Scanner sc = new Scanner(System.in); // this is the second line needed to add the scanner
@@ -15,20 +15,20 @@ int b = sc.nextInt();
 
 if (b == 1) {
     System.out.println("u want to plus it");
-}
-else {
-    if (b == 2) {
+    }
+
+    else if (b == 2) {
     System.out.println("u want to minus it");
     }
-    else {
-        if (b == 3) {
-        System.out.println("u want to times it");
- {
-    System.out.println("u didnt do 1 2 3");
-}
-        }
+ 
+    else if (b == 3) {
+    System.out.println("u want to times it");
     }
-}
+
+    else {
+    System.out.println("u failed how");
+    System.exit(0);
+    }
     
     
 
@@ -41,16 +41,13 @@ System.out.println("Second Number: " + c);
 if (b == 1) {
     System.out.println(a + c);
 }
-if (b == 2) {
+else if (b == 2) {
     System.out.println(a - c);
 }
-if (b == 3) {
+else if (b == 3) {
     System.out.println(a * c);
 }
 
-else {
-    System.out.println("u dumb dog");
-}
 
 sc.close(); // put this at the end of scanners or bugs
     }
